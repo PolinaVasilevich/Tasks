406693a7e8b360915abeaa958b015b7c45dfa566
Polina Vasilevich <polja.vasilevich@gmail.com>
2019-08-06 22:01:58 +0300

    Added new func

--- FirstTask.py
+++ FirstTask.py
@@ -10,11 +10,17 @@ def area(a, b):
 
 
 def perimeter(a, b):
-    return a + b
+    return (a + b) * 2
+
+
+def dictionary(a, b):
+    return {'Diagonal': diagonal(a, b), 'Area': area(a, b), 'Perimeter': perimeter(a, b)}
 
 
 if __name__ == '__main__':
     width = int(input('Width = '))
     length = int(input('Length = '))
-    print("Diagonal = {} \nArea  = {} \nPerimeter = {}".format(diagonal(width, length),
-                                                               area(width, length), perimeter(width, length)))
+    print("Diagonal = {} \nArea  = {} \nPerimeter = {} \nDictionary = {}".format(diagonal(width, length),
+                                                                                 area(width, length),
+                                                                                 perimeter(width, length),
+                                                                                 dictionary(width, length)))
