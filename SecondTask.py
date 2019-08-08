310053e8acf85076b2c42006b973e4694a9edbef
Polina Vasilevich <polja.vasilevich@gmail.com>
2019-08-08 22:55:29 +0300

    Merged second task into one func

--- SecondTask.py
+++ SecondTask.py
@@ -1,22 +1,18 @@
 import random
 
 
-def string_conversion(s):
+def magic_function(s):
     letter = set('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ')
     a = []
     for i in range(len(s)):
         if s[i] in letter:
             a.append(s[i])
-    return ''.join(a)
-
-
-def magic_function(s):
-    s = s.split()
+    con_str = ''.join(a).split()
     b = []
-    for i in range(len(s)):
-        b.append(''.join(list(s[i])[::-1]))
+    for i in range(len(con_str)):
+        b.append(''.join(list(con_str[i])[::-1]))
     return ' '.join(random.sample(b, len(b)))
 
 
 if __name__ == '__main__':
-    print(magic_function(string_conversion('07neves 08thgie 2ytnewt 19neetenin')))
+    print(magic_function('07neves 08thgie 2ytnewt 19neetenin'))
