e4aa4279c1c79b5a96828cabb8bd47b9f7194427
Polina Vasilevich <polja.vasilevich@gmail.com>
2019-08-10 23:16:40 +0300

    Changed magic_function

--- SecondTask.py
+++ SecondTask.py
@@ -1,18 +1,16 @@
+import string
 import random
 
 
 def magic_function(s):
-    letter = set('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ')
-    a = []
-    for i in range(len(s)):
-        if s[i] in letter:
-            a.append(s[i])
-    con_str = ''.join(a).split()
-    b = []
-    for i in range(len(con_str)):
-        b.append(''.join(list(con_str[i])[::-1]))
-    return ' '.join(random.sample(b, len(b)))
+    s = s.split()
+    for i, word in enumerate(s):
+        for letter in word:
+            if letter not in string.ascii_letters:
+                word = word.replace(letter, '')
+        s[i] = word[::-1]
+    return ' '.join(random.sample(s, len(s)))
 
 
 if __name__ == '__main__':
-    print(magic_function('07neves 08thgie 2ytnewt 19neetenin'))
+    print(magic_function('5ADadc1!!! s6d2,./ dv6/v3'))
